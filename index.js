a744c60004072dd2da4fa268dcdd68f088fc7e7a
Jason Karns <jason.karns@gmail.com>
2020-03-18 13:43:13 -0400

    Ensure errors are caught
    This is some weird wonkery, but it allows the main entrypoint to catch
    rejections (thus avoiding UnhandledRejection exceptions);
    while at the same time allowing the un-caught promise to be exported in
    such a way that other modules may require it directly.
    
    The error utility is only necessary because core.setFailed doesn't
    accept Errors as an argument.

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const exec = require('util').promisify(require('child_process').exec)
 const core = require('@actions/core')
 
 const setupNodenv = require('../setup-nodenv')
+const { fail } = require('../src/utils')
 
 function run () {
   return setupNodenv
     .then(() => exec('nodenv local'))
     .then(cmd => core.setOutput('node-version', cmd.stdout.trim()))
-    .catch(err => core.setFailed(err.message))
 }
 
 module.exports = run()
+module.exports.catch(fail)
